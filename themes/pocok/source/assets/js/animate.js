35f2814aa02d5b3168ae97b986c0b2eb0ae2fb16
Eduard Pochtar <iamfrntdv@gmail.com>
2018-03-28 12:12:54 +0200

    hero terminal update

---
+++ themes/pocok/source/assets/js/animate.js
@@ -0,0 +1,77 @@
+
+function animate(typewriter) {
+    sequence = [
+        {
+            text: ":~ $ ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "pocok init my-project\n",
+            delayAfter: 500
+        },
+        {
+            text: "pocok.yaml created\ndocker-compose.yaml created\nProject init completed!\n",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: ":~ $ ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "pocok up\n",
+            delayAfter: 1000
+        },
+        {
+            text: "Pulling helloworld (ubuntu:14.04)...\n",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "14.04: Pulling from library/ubuntu\n",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "Digest: sha256:ed4903ds63459d6e5ed6c0f23d45e94c3a0t50d24727c7c5c48fded60f70aa96\n",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "Status: Image is up to date for ubuntu:14.04\n",
+            instant: true,
+            delayAfter: 1000
+        },
+        {
+            text: "services:\n  helloworld:\n    command: /bin/echo 'Hello world'\n    image: ubuntu:14.04\nversion: '2.0'\n",
+            instant: true,
+            delayAfter: 1000
+        },
+        {
+            text: "Creating network \"ttt_default\" with the default driver\n",
+            instant: true,
+            delayAfter: 1000
+        },
+        {
+            text: "Creating my-project_helloworld_1 ... done\n",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "      Name                Command          State    Ports\n"
+            + "---------------------------------------------------------\n"
+            +"ttt_helloworld_1   /bin/echo Hello world   Exit 0",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "\n",
+            instant: true,
+            delayAfter: 2000
+        }
+    ];
+
+    typewriter.playSequence(sequence);
+}
